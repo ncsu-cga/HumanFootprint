b2537696be205b08f7f7c796152e755a9b88f1eb
jbvogler <56692075+jbvogler@users.noreply.github.com>
2020-01-14 14:33:48 -0500

    Update uniquevalue.js

--- assets/js/uniquevalue.js
+++ assets/js/uniquevalue.js
@@ -23,22 +23,22 @@ const uniquevalue = {
     {"value": "Very low density", "rgba": [225, 225, 225, 1], "hex": "#E1E1E1"}
   ],
   "HDTRAN0010": [
-    {"value": "High density to High density", "rgba": [8, 81, 156, 1], "hex": "#08519c"},
-    {"value": "High density to Medium density", "rgba": [49, 130, 189, 1], "hex": "#3182bd"},
-    {"value": "High density to Low density", "rgba": [107, 174, 214, 1], "hex": "#6baed6"},
-    {"value": "High density to Very low density", "rgba": [189, 215, 231, 1], "hex": "#bdd7e7"},
-    {"value": "Medium density to High density", "rgba": [166, 54, 3, 1], "hex": "#a63603"},
-    {"value": "Medium density to Medium density", "rgba": [230, 85, 13, 1], "hex": "#e6550d"},
-    {"value": "Medium density to Low density", "rgba": [253, 141, 60, 1], "hex": "#fd8d3c"},
-    {"value": "Medium density to Very low density", "rgba": [253, 190, 133, 1], "hex": "#fdbe85"},
-    {"value": "Low density to High density", "rgba": [84, 39, 143, 1], "hex": "#54278f"},
-    {"value": "Low density to Medium density", "rgba": [117, 107, 177, 1], "hex": "#756bb1"},
-    {"value": "Low density to Low density", "rgba": [158, 154, 200, 1], "hex": "#9e9ac8"},
-    {"value": "Low density to Very low density", "rgba": [203, 201, 226, 1], "hex": "#cbc9e2"},
-    {"value": "Very low density to High density", "rgba": [0, 109, 44, 1], "hex": "#006d2c"},
-    {"value": "Very low density to Medium density", "rgba": [49, 163, 84, 1], "hex": "#31a354"},
-    {"value": "Very low density to Low density", "rgba": [116, 196, 118, 1], "hex": "#74c476"},
-    {"value": "Very low density to Very low density", "rgba": [186, 228, 179, 1], "hex": "#bae4b3"}
+    {"value": "High density to High density", "rgba": [8, 81, 156, 1], "hex": "#08519C"},
+    {"value": "High density to Medium density", "rgba": [49, 130, 189, 1], "hex": "#3182BD"},
+    {"value": "High density to Low density", "rgba": [107, 174, 214, 1], "hex": "#6BAED6"},
+    {"value": "High density to Very low density", "rgba": [189, 215, 231, 1], "hex": "#BDD7E7"},
+    {"value": "Medium density to High density", "rgba": [166, 54, 3, 1], "hex": "#A63603"},
+    {"value": "Medium density to Medium density", "rgba": [230, 85, 13, 1], "hex": "#E6550D"},
+    {"value": "Medium density to Low density", "rgba": [253, 141, 60, 1], "hex": "#FD8D3C"},
+    {"value": "Medium density to Very low density", "rgba": [253, 190, 133, 1], "hex": "#FDBE85"},
+    {"value": "Low density to High density", "rgba": [84, 39, 143, 1], "hex": "#54278F"},
+    {"value": "Low density to Medium density", "rgba": [117, 107, 177, 1], "hex": "#756BB1"},
+    {"value": "Low density to Low density", "rgba": [158, 154, 200, 1], "hex": "#9E9AC8"},
+    {"value": "Low density to Very low density", "rgba": [203, 201, 226, 1], "hex": "#CBC9E2"},
+    {"value": "Very low density to High density", "rgba": [0, 109, 44, 1], "hex": "#006D2C"},
+    {"value": "Very low density to Medium density", "rgba": [49, 163, 84, 1], "hex": "#31A354"},
+    {"value": "Very low density to Low density", "rgba": [116, 196, 118, 1], "hex": "#74C476"},
+    {"value": "Very low density to Very low density", "rgba": [186, 228, 179, 1], "hex": "#BAE4D3"}
   ]
 }
 
